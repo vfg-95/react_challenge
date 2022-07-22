6296bc87091a6bc32e8f88353488b75b18a23e2a
vfg-95 <sam.ridding@hotmail.co.uk>
2022-07-22 09:35:33 +0000

    Content challenge

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,13 @@
 import css from './App.module.css';
 import Sidebar from './components/Sidebar';
 import NavBarSimple from './components/NavBarSimple';
+import NavBarForm from './components/NavBarForm';
+import Content from './components/Content';
 
 function App() {
   return (
     <div className={css.App}>
-      <NavBarSimple />
+      <Content />
     </div>
   );
 }
---
+++ my-app/src/components/Content.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+import css from  "./css/Content.module.css"
+import {savedPosts} from "../posts.json";
+import PostItem from "./PostItem";
+
+export class Content extends Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+      return (
+      <div className={css.Content}>
+        <div className={CSS.TitleBar}>
+          <h1>My Photos</h1>
+        </div>
+        <div className={css.SearchResults}>
+          <PostItem savedPosts={savedPosts} />
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Content
